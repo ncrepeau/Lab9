87dd49d294a14ddf681444ae1df1018207edcdb0
ncrep <ncrep@DESKTOP-2ULBP3G>
2018-11-10 14:01:32 -0700

    Skeleton Driver

--- CSCI 136 Lab 9/src/Driver.java
+++ CSCI 136 Lab 9/src/Driver.java
@@ -1,11 +1,45 @@
 import java.io.FileNotFoundException;
+import java.util.Random;
 
-public class Driver {
+import javafx.application.Application;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
-	public static void main(String[] args) throws FileNotFoundException {
+public class Driver extends Application{
+
+	public static void main(String[] args){
 		// TODO Auto-generated method stub
+
+		
+
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws FileNotFoundException {
+		// TODO Auto-generated method stub
+		GridPane myPane = new GridPane();
 		MazeCreation myMaze = new MazeCreation();
-		int[][] array1 = myMaze.array1();
+		int[][] myArray;
+		Random rand = new Random();
+		int randNum = rand.nextInt(((2 - 1) + 1) + 1);
+		if (randNum == 1) {
+			myArray = myMaze.array1();
+		} else {
+			myArray = myMaze.array2();
+		}
+		for(int i = 0; i < myArray.length; i++)
+		{
+			for(int j = 0; j < myArray[i].length; j++)
+			{
+				if(myArray[i][j] == 1) {
+					myPane.add(null, i, j); //Add in the image Path for the blocks here
+				}else {
+					break;//spaces the player can move through
+				}
+			}
+			
+		}
+		
 		
 	}
 }
--- CSCI 136 Lab 9/src/MazeCreation.java
+++ CSCI 136 Lab 9/src/MazeCreation.java
@@ -1,43 +1,36 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Random;
 import java.util.Scanner;
 
 public class MazeCreation {
-	Random rand = new Random();
 	private int[][] array1 = new int[5][10];
 	private int[][] array2 = new int[5][10];
 
 	public int[][] array1() throws FileNotFoundException {
-		int randNum = rand.nextInt(((2 - 1) + 1) + 1);
-		if (randNum == 1) {
-			File myFile = new File("./src/Maze1.txt");
-			Scanner myFileScanner = new Scanner(myFile);
-			for (int row = 0; row < array1.length; row++) {
-				String line = myFileScanner.nextLine();
-				Scanner myLineScanner = new Scanner(line);
-				for (int column = 0; column < array1[row].length; column++) {
-					array1[row][column] = myLineScanner.nextInt();
-				}
-			}
+		File myFile = new File("./src/Maze1.txt");
+		Scanner myFileScanner = new Scanner(myFile);
+		for (int row = 0; row < array1.length; row++) {
+			String line = myFileScanner.nextLine();
+			Scanner myLineScanner = new Scanner(line);
+			for (int column = 0; column < array1[row].length; column++) {
+				array1[row][column] = myLineScanner.nextInt();
 
-			return array1;
-
-		}
-		if (randNum == 2) {
-			{
-				File myFile = new File("./src/Maze2.txt");
-				Scanner myFileScanner = new Scanner(myFile);
-				for (int row = 0; row < array2.length; row++) {
-					String line = myFileScanner.nextLine();
-					Scanner myLineScanner = new Scanner(line);
-					for (int column = 0; column < array2[row].length; column++) {
-						array2[row][column] = myLineScanner.nextInt();
-					}
-				}
-
-				return array2;
 			}
 		}
+		return array1;
+	}
+
+	public int[][] array2() throws FileNotFoundException {
+		File myFile = new File("./src/Maze2.txt");
+		Scanner myFileScanner = new Scanner(myFile);
+		for (int row = 0; row < array2.length; row++) {
+			String line = myFileScanner.nextLine();
+			Scanner myLineScanner = new Scanner(line);
+			for (int column = 0; column < array2[row].length; column++) {
+				array2[row][column] = myLineScanner.nextInt();
+			}
+		}
+
+		return array2;
 	}
 }
