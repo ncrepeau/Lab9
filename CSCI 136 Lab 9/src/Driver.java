fde98193cf29d73c07af9ccb7e63da32f70160bb
ncrep <ncrep@DESKTOP-2ULBP3G>
2018-11-10 13:25:10 -0700

    i hate classes

--- CSCI 136 Lab 9/src/Driver.java
+++ CSCI 136 Lab 9/src/Driver.java
@@ -5,17 +5,7 @@ public class Driver {
 	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
 		MazeCreation myMaze = new MazeCreation();
-		int[][] myArray = myMaze.array();
-		for(int i = 0; i < myArray.length; i++)
-		{
-			for(int j = 0; j < myArray[i].length; j++)
-			{
-				System.out.print(myArray[i][j] + "\t");
-			}
-			System.out.println();
-		}
-	}
+		int[][] array1 = myMaze.array1();
 		
-		}
-	
-
+	}
+}
--- CSCI 136 Lab 9/src/MazeCreation.java
+++ CSCI 136 Lab 9/src/MazeCreation.java
@@ -5,22 +5,39 @@ import java.util.Scanner;
 
 public class MazeCreation {
 	Random rand = new Random();
-	int[][] myArray = new int[5][10];
-	
+	private int[][] array1 = new int[5][10];
+	private int[][] array2 = new int[5][10];
 
-	public int[][] array() throws FileNotFoundException {
-		
-		File myFile = new File("./src/Maze1.txt");
-		Scanner myFileScanner = new Scanner(myFile);
-		for (int row = 0; row < myArray.length; row++) {
-		    String line = myFileScanner.nextLine();
-		    Scanner myLineScanner = new Scanner(line);
-		    for (int column = 0; column < myArray[row].length; column++) {
-		        myArray[row][column] = myLineScanner.nextInt();
-		    }
+	public int[][] array1() throws FileNotFoundException {
+		int randNum = rand.nextInt(((2 - 1) + 1) + 1);
+		if (randNum == 1) {
+			File myFile = new File("./src/Maze1.txt");
+			Scanner myFileScanner = new Scanner(myFile);
+			for (int row = 0; row < array1.length; row++) {
+				String line = myFileScanner.nextLine();
+				Scanner myLineScanner = new Scanner(line);
+				for (int column = 0; column < array1[row].length; column++) {
+					array1[row][column] = myLineScanner.nextInt();
+				}
+			}
+
+			return array1;
+
 		}
-		
-	
-		return myArray;
+		if (randNum == 2) {
+			{
+				File myFile = new File("./src/Maze2.txt");
+				Scanner myFileScanner = new Scanner(myFile);
+				for (int row = 0; row < array2.length; row++) {
+					String line = myFileScanner.nextLine();
+					Scanner myLineScanner = new Scanner(line);
+					for (int column = 0; column < array2[row].length; column++) {
+						array2[row][column] = myLineScanner.nextInt();
+					}
+				}
+
+				return array2;
+			}
+		}
 	}
 }
