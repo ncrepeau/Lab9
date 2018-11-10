7acd05c567c583d4528835a408661b133b8e5d2a
ncrep <ncrep@DESKTOP-2ULBP3G>
2018-11-10 13:06:24 -0700

    Array is working

--- CSCI 136 Lab 9/src/Driver.java
+++ CSCI 136 Lab 9/src/Driver.java
@@ -5,7 +5,7 @@ public class Driver {
 	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
 		MazeCreation myMaze = new MazeCreation();
-		int[][] myArray = myMaze.Array();
+		int[][] myArray = myMaze.array();
 		for(int i = 0; i < myArray.length; i++)
 		{
 			for(int j = 0; j < myArray[i].length; j++)
--- CSCI 136 Lab 9/src/MazeCreation.java
+++ CSCI 136 Lab 9/src/MazeCreation.java
@@ -1,28 +1,26 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Random;
 import java.util.Scanner;
 
 public class MazeCreation {
+	Random rand = new Random();
 	int[][] myArray = new int[5][10];
+	
 
-	public int[][] Array() throws FileNotFoundException {
-
+	public int[][] array() throws FileNotFoundException {
+		
 		File myFile = new File("./src/Maze1.txt");
 		Scanner myFileScanner = new Scanner(myFile);
-		while (myFileScanner.hasNextLine()) {
-			String line = myFileScanner.nextLine();
-			Scanner myLineScanner = new Scanner(line);
-			int row = 0;
-			while (myLineScanner.hasNext()) {
-			    int x = myLineScanner.nextInt();
-			    for (int column = 0; column < myArray[0].length; column++) {
-			        myArray[row][column] = x;
-
-			    }
-			    row =+ 1;
-			}
-
+		for (int row = 0; row < myArray.length; row++) {
+		    String line = myFileScanner.nextLine();
+		    Scanner myLineScanner = new Scanner(line);
+		    for (int column = 0; column < myArray[row].length; column++) {
+		        myArray[row][column] = myLineScanner.nextInt();
+		    }
 		}
+		
+	
 		return myArray;
 	}
 }
